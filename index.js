40190e0301771ebf30ac7e71f6bcc9a4d91a2ffb
Nicholas <crazedglitch@yahoo.com>
2023-03-18 15:31:08 -0500

    lab in progress

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const saturdayFun = activity => activity ? 'This Saturday, I want to bathe my dog!' : 'This Saturday, I want to roller-skate!';
+
+const mondayWork = activity => activity ? "This Monday, I will work from home." : "This Monday, I will go to the office.";
+
+function wrapAdjective(special) {
+    console.log*(`You are ${special}`)
+    const extraFunction = wrapAdjective("*")
+    return extraFunction()
+}
+
+
+wrapAdjective("special")
